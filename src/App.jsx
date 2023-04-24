541d8c858f001994c8417871d7770aa3fd35291f
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-04-24 17:54:49 -0300

    :sparkles: feat: Fianlizado estilização e responsividade pag Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
 
   return (
-    <main>Teste</main>
-  )
+    <main className='main'>
+      <Home />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './home.css';
+
+const Home = () => {
+  return (
+    <section className='home column'>
+        <div className="home__content column">
+          <h2 className='home__subtitle'>SO, YOU WANT TO TRAVEL TO</h2>
+          <h1 className='home__title'>Space</h1>
+          <p className="home__text">
+            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
+        <button className='home__button' type='button'>EXPLORE</button>
+    </section>
+  )
+};
+
+export default Home;
